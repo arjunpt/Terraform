cfcf052a104cab99b3fee6fc1064ec8dd982d755
Arjun PT <52270351+arjunpt@users.noreply.github.com>
2023-10-16 06:33:49 -0700

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "abhishek-s3-demo-xyz" # we need a bucket, region and encryption 
     key            = "abhi/terraform.tfstate" #if we ahve any prefix to the file terraform.tfstate
-                                              #once configured state file will created at the s3 bucket
+                                              #once configured state file will created at the s3 bucket, so an folder abhi will create and inside it there contain state file
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
